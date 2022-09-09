248da39a22251cb8ade16d341f39c4a2abff0092
seebaastiian <seba7934rojas@gmail.com>
2022-09-09 11:30:12 -0300

    confirmar

---
+++ prediccion2_.js
@@ -0,0 +1,39 @@
+function muestraInformaciónContacto() {
+    var auntContactInfo = ["Paula", "Smith", "Calle Principal 1234", "St. Louis", "MO", 12345];
+    console.log(auntContactInfo);
+
+//console.log imprime [paula,smith,calle principal 1234,st. louis,mo,12345]
+/*
+diagrama en T 
+variables : auntcontactinfo | valores : [Paula", "Smith", "Calle Principal 1234", "St. Louis", "MO", 12345];
+*/
+
+
+
+
+
+function muestraListaDeCompras() {
+    var produce = ["manzanas", "naranjas"];
+    var frozen = ["brócoli", "helado"];
+    frozen.push("croqueta de papa");
+    console.log(frozen);
+}
+//console. log imprime [brócoli,helado,croqueta de papa]
+/*
+diagrama en T 
+variables : produce : | valores [manzanas,naranjas]
+variables : forzen : | valores [brócoli,helado]
+*/
+
+
+
+
+var movieLibrary = ["Bambi", "E.T.", "Toy Story"];
+movieLibrary.push("Zoro");
+movieLibrary[1] = "Beetlejuice";
+console.log(movieLibrary);
+//console.log imprime [bambi,beetlejuice,toy story,zoro]
+/*
+diagrama en T
+variable movielibrary : | valores : [bambi, e.t. , toy story]
+*/
